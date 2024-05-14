0e1a0d5c4f08bedec5b9087c078189df82e2583a
Karan Choudhary <66177194+karan123789@users.noreply.github.com>
2024-05-14 18:00:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,44 @@ import pickle
 import streamlit as st
 import requests
 import random
+import numpy as np
+import pandas as pd
+import ast
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
+# Load movie data
+movies = pd.read_csv('tmdb_5000_movies.csv')
+credits = pd.read_csv('tmdb_5000_credits.csv') 
+
+# Merge dataframes and preprocess data
+movies = movies.merge(credits, on='title')
+movies = movies[['movie_id','title','overview','genres','keywords','cast','crew']]
+movies.dropna(inplace=True)
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
+movies['cast'] = movies['cast'].apply(convert)
+movies['crew'] = movies['crew'].apply(fetch_director)
+movies['cast'] = movies['cast'].apply(lambda x: x[0:3])
+movies['genres'] = movies['genres'].apply(collapse)
+movies['keywords'] = movies['keywords'].apply(collapse)
+movies['cast'] = movies['cast'].apply(collapse)
+movies['crew'] = movies['crew'].apply(collapse)
+movies['overview'] = movies['overview'].apply(lambda x: x.split())
+movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+new = movies.drop(columns=['overview','genres','keywords','cast','crew'])
+new['tags'] = new['tags'].apply(lambda x: " ".join(x))
+
+# Generate similarity matrix
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vector = cv.fit_transform(new['tags']).toarray()
+similarity = cosine_similarity(vector)
+
+# Save data to pickle files
+pickle.dump(new, open('movie_list.pkl', 'wb'))
+pickle.dump(similarity, open('similarity.pkl', 'wb'))
+
+# Define remaining functions
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
     data = requests.get(url)
@@ -15,52 +52,21 @@ def fetch_poster(movie_id):
     return full_path
 
 def recommend(movie):
-    try:
-        # Check if 'genres' column exists in movies DataFrame
-        if 'genres' in movies.columns:
-            # Get the genre of the selected movie
-            selected_movie_genre = movies[movies['title'] == movie]['genres'].values[0]
+    index = new[new['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movies = [new.iloc[i[0]] for i in distances[1:6]]
+    return recommended_movies
 
-            # Filter movies by genre (content-based filtering)
-            similar_movies = movies[movies['genres'].apply(lambda x: selected_movie_genre in x)]
-
-            if len(similar_movies) > 5:
-                # Get top 5 similar movies
-                recommended_movie_names = similar_movies['title'].values[:5]
-                recommended_movie_posters = [fetch_poster(movie_id) for movie_id in similar_movies['movie_id'].values[:5]]
-            else:
-                recommended_movie_names = similar_movies['title'].values
-                recommended_movie_posters = [fetch_poster(movie_id) for movie_id in similar_movies['movie_id'].values]
-
-        else:
-            # If 'genres' column is missing, recommend random movies
-            random_movies = random.sample(list(movies['title']), 5)
-            recommended_movie_names = random_movies
-            recommended_movie_posters = [fetch_poster(random.choice(movies['movie_id'])) for _ in range(5)]
-
-    except Exception as e:
-        st.error("Error occurred while recommending movies.")
-        st.error(str(e))
-        recommended_movie_names = []
-        recommended_movie_posters = []
-
-    return recommended_movie_names, recommended_movie_posters
-
+# Streamlit app
 st.header('Movie Recommender System')
 
-# Load movie list (assuming this file still exists)
-movies = pickle.load(open('movie_list.pkl', 'rb'))
-movie_list = movies['title'].values
-
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
+# Dropdown to select a movie
+selected_movie = st.selectbox("Type or select a movie from the dropdown", new['title'].values)
 
 if st.button('Show Recommendation'):
-    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    recommended_movies = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.columns(5)
-    for i in range(len(recommended_movie_names)):
+    for recommended_movie in recommended_movies:
         with st.container():
-            st.text(recommended_movie_names[i])
-            st.image(recommended_movie_posters[i])
+            st.text(recommended_movie['title'])
+            st.image(fetch_poster(recommended_movie['movie_id']))
