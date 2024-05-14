69389f08a773471511a7ca29c1986d168b1ec00b
Karan Choudhary <66177194+karan123789@users.noreply.github.com>
2024-05-14 18:39:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,85 +1,69 @@
 import pickle
 import streamlit as st
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-# Load movie list (assuming this file still exists)
-movies = pd.read_csv('tmdb_5000_movies.csv')
-movies = movies[['id', 'original_title', 'overview', 'genres', 'keywords', 'cast']]
-
-def convert(text):
-    L = []
-    for i in ast.literal_eval(text):
-        L.append(i['name']) 
-    return L 
-
-def convert3(text):
-    L = []
-    counter = 0
-    for i in ast.literal_eval(text):
-        if counter < 3:
-            L.append(i['name'])
-        counter += 1
-    return L 
-
-def fetch_director(text):
-    L = []
-    for i in ast.literal_eval(text):
-        if i['job'] == 'Director':
-            L.append(i['name'])
-    return L 
-
-def collapse(L):
-    L1 = []
-    for i in L:
-        L1.append(i.replace(" ",""))
-    return L1
-
-movies.dropna(inplace=True)
-movies['genres'] = movies['genres'].apply(convert)
-movies['keywords'] = movies['keywords'].apply(convert)
-movies['cast'] = movies['cast'].apply(convert3)
-movies['cast'] = movies['cast'].apply(lambda x: x[0:3])
-movies['cast'] = movies['cast'].apply(collapse)
-movies['genres'] = movies['genres'].apply(collapse)
-movies['keywords'] = movies['keywords'].apply(collapse)
-movies['overview'] = movies['overview'].apply(lambda x: x.split())
-movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast']
-new = movies.drop(columns=['overview', 'genres', 'keywords', 'cast'])
-new['tags'] = new['tags'].apply(lambda x: " ".join(x))
-
-# Calculate cosine similarity
-cv = CountVectorizer(max_features=5000, stop_words='english')
-vector = cv.fit_transform(new['tags']).toarray()
-similarity = cosine_similarity(vector)
+import requests
+import random
+import numpy as np
 
 def fetch_poster(movie_id):
-    return f"https://image.tmdb.org/t/p/w500/{movie_id}"
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    if 'poster_path' in data:
+        poster_path = data['poster_path']
+        full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    else:
+        full_path = "https://via.placeholder.com/500x750.png?text=Poster+Not+Available"
+    return full_path
+
+def calculate_similarity(selected_genre, movie_genre):
+    # Calculate Jaccard similarity between two genre lists
+    selected_set = set(selected_genre)
+    movie_set = set(movie_genre)
+    intersection = len(selected_set.intersection(movie_set))
+    union = len(selected_set.union(movie_set))
+    similarity = intersection / union if union > 0 else 0
+    return similarity
 
 def recommend(movie):
     try:
-        index = new[new['original_title'] == movie].index[0]
-        distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+        # Check if 'genres' column exists in movies DataFrame
+        if 'genres' in movies.columns:
+            # Get the genre of the selected movie
+            selected_movie_genre = movies[movies['title'] == movie]['genres'].values[0]
 
-        recommended_movies = []
-        recommended_posters = []
+            # Calculate similarity scores for all movies based on genre
+            movies['similarity'] = movies['genres'].apply(lambda x: calculate_similarity(selected_movie_genre, x))
 
-        for i in distances[1:6]:
-            recommended_movies.append(new.iloc[i[0]].original_title)
-            recommended_posters.append(fetch_poster(new.iloc[i[0]].id))
+            # Sort movies by similarity score in descending order
+            similar_movies = movies.sort_values(by='similarity', ascending=False)
+
+            if len(similar_movies) > 5:
+                # Get top 5 similar movies
+                recommended_movie_names = similar_movies['title'].values[1:6]
+                recommended_movie_posters = [fetch_poster(movie_id) for movie_id in similar_movies['movie_id'].values[1:6]]
+            else:
+                recommended_movie_names = similar_movies['title'].values[1:]
+                recommended_movie_posters = [fetch_poster(movie_id) for movie_id in similar_movies['movie_id'].values[1:]]
+
+        else:
+            # If 'genres' column is missing, recommend random movies
+            random_movies = random.sample(list(movies['title']), 5)
+            recommended_movie_names = random_movies
+            recommended_movie_posters = [fetch_poster(random.choice(movies['movie_id'])) for _ in range(5)]
 
     except Exception as e:
         st.error("Error occurred while recommending movies.")
         st.error(str(e))
-        recommended_movies = []
-        recommended_posters = []
+        recommended_movie_names = []
+        recommended_movie_posters = []
 
-    return recommended_movies, recommended_posters
+    return recommended_movie_names, recommended_movie_posters
 
 st.header('Movie Recommender System')
 
-movie_list = new['original_title'].values
+# Load movie list (assuming this file still exists)
+movies = pickle.load(open('movie_list.pkl', 'rb'))
+movie_list = movies['title'].values
 
 selected_movie = st.selectbox(
     "Type or select a movie from the dropdown",
