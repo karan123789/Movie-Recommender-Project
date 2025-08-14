d6cb4aa2f98bb86c74c3885b3c1fb86d5afa3a9d
Karan Choudhary <karanc123789@gmail.com>
2025-08-14 18:30:41 -0400

    updated recommendations

--- app.py
+++ app.py
@@ -18,58 +18,21 @@ def fetch_poster(movie_id):
     return full_path
 
 def recommend(movie):
-    try:
-        # Check if necessary columns exist in movies DataFrame
-        if 'genres' in movies.columns and 'keywords' in movies.columns and 'tags' in movies.columns:
-            # Get genre, keywords, and tags of the selected movie
-            selected_movie_genre = movies[movies['title'] == movie]['genres'].values[0]
-            selected_movie_keywords = movies[movies['title'] == movie]['keywords'].values[0]
-            selected_movie_tags = movies[movies['title'] == movie]['tags'].values[0]
-
-
-            movies['combined_features'] = (movies['genres'].fillna('') + ' ' + 
-                                          movies['genres'].fillna('') + ' ' + 
-                                          movies['keywords'].fillna('') + ' ' + 
-                                          movies['tags'].fillna(''))
-
-            # Create TF-IDF vectors for combined features
-            tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_features=5000, ngram_range=(1, 2))
-            tfidf_matrix = tfidf_vectorizer.fit_transform(movies['combined_features'])
-
-            # Compute cosine similarity between selected movie and all other movies
-            selected_movie_index = movies[movies['title'] == movie].index[0]
-            cosine_similarities = cosine_similarity(tfidf_matrix[selected_movie_index:selected_movie_index+1], tfidf_matrix).flatten()
-
-            
-            similar_movies_indices = cosine_similarities.argsort()[::-1][1:21] 
-            similar_movies = movies.iloc[similar_movies_indices]
-
-            if len(similar_movies) > 5:
-                # Get top 5 similar movies
-                recommended_movie_names = similar_movies['title'].values[:5]
-                recommended_movie_posters = [fetch_poster(movie_id) for movie_id in similar_movies['movie_id'].values[:5]]
-            else:
-                recommended_movie_names = similar_movies['title'].values
-                recommended_movie_posters = [fetch_poster(movie_id) for movie_id in similar_movies['movie_id'].values]
-
-        else:
-            # If required columns are missing, recommend random movies
-            random_movies = random.sample(list(movies['title']), 5)
-            recommended_movie_names = random_movies
-            recommended_movie_posters = [fetch_poster(random.choice(movies['movie_id'])) for _ in range(5)]
-
-    except Exception as e:
-        st.error("Error occurred while recommending movies.")
-        st.error(str(e))
-        recommended_movie_names = []
-        recommended_movie_posters = []
-
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
     return recommended_movie_names, recommended_movie_posters
 
 st.header('Movie Recommender System')
 
 # Load movie list (assuming this file still exists)
 movies = pickle.load(open('movie_list.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl.gz', 'rb'))
 movie_list = movies['title'].values
 
 selected_movie = st.selectbox(
